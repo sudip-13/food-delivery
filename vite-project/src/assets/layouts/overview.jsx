7805a5abd4508f54dba003c3b3a38dfd1516a067
Mukesh Kr. Gupta <rg37090@gmail.com>
2024-01-26 12:35:36 +0530

    overview done

--- vite-project/src/assets/layouts/overview.jsx
+++ vite-project/src/assets/layouts/overview.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
 import "../styles/overview.css";
 import Cookies from "js-cookie";
 
 function Overview() {
+  const [orderDetails, setOrderDetails] = useState('');
+  const [total, setTotal] = useState(0);
+
   const fetchCartAndShow = async (e) => {
-    e.preventDefault();
     let email = null;
     const token = Cookies.get("userlogincookie");
     try {
@@ -16,9 +18,12 @@ function Overview() {
         });
       try {
         await axios
-          .post("http://localhost:3001/user/getcart", { email})
+          .post("http://localhost:3001/user/getcart", { email })
           .then((result) => {
             console.log(result.data);
+            const items = result.data;
+            setOrderDetails(items);
+            calculateTotal(items);
           });
       } catch (error) {
         console.error("Internal server error !", error);
@@ -28,26 +33,36 @@ function Overview() {
     }
   };
 
+  const calculateTotal = (orderDetails) => {
+    let totalPrice = 0;
+    Object.keys(orderDetails).forEach((item) => {
+      const pricePerItem = 10;
+      totalPrice += orderDetails[item] * pricePerItem;
+    });
+    setTotal(totalPrice);
+  };
+  useEffect(() => {
+    fetchCartAndShow();
+  }, []);
+
   return (
-    <div className="overview">
-      <div className="App">
-        <header className="App-header">
-          <h1>Food Delivery App</h1>
-        </header>
-        <div className="order-container">
-          <h2>Order Overview</h2>
-          {/* <ul>
-            {items.map((item) => (
-              <li key={item.id}>
-                {item.name} - ${item.price}
-              </li>
-            ))}
-          </ul> */}
-          <div className="subtotal">
-            {/* <h3>Subtotal: ${calculateSubtotal()}</h3> */}
-            <button onClick={fetchCartAndShow}>Make Payment</button>
+    <div className="order-details-container">
+      <h2>Order Details</h2>
+
+      <div className="order-items">
+        {Object.keys(orderDetails).map((item) => (
+          <div key={item} className="order-item">
+            <span>{item}</span>
+            <span>Quantity: {orderDetails[item]}</span>
+            <span>Price: ${orderDetails[item] * 10}</span>
           </div>
-        </div>
+        ))}
+      </div>
+      <div className="total">
+        <h3>Total: ${total}</h3>
+      </div>
+      <div className="overview_btn">
+        <button>make payment</button>
       </div>
     </div>
   );
